b616055914d991cda31f0d97f5642100df9db08e
alex.mindustry <alex.mindustry@gmail.com>
2020-12-23 12:01:27 +0800

    Initial commit

---
+++ src/alexhub/AlexHubPlugin.java
@@ -0,0 +1,71 @@
+package alexhub;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.net.Administration.*;
+import mindustry.world.blocks.storage.*;
+
+public class AlexHubPlugin extends Plugin{
+
+    //called when game initializes
+    @Override
+    public void init(){
+        //listen for a block selection event
+
+
+        //add an action filter for preventing players from doing certain things
+        Vars.netServer.admins.addActionFilter(action -> {
+            //random example: prevent blast compound depositing
+            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
+                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
+                return false;
+            }
+            return true;
+        });
+    }
+
+    //register commands that run on the server
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+        handler.register("reactors", "List all thorium reactors in the map.", args -> {
+            for(int x = 0; x < Vars.world.width(); x++){
+                for(int y = 0; y < Vars.world.height(); y++){
+                    //loop through and log all found reactors
+                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor){
+                        Log.info("Reactor at @, @", x, y);
+                    }
+                }
+            }
+        });
+    }
+
+    //register commands that player can invoke in-game
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+
+        //register a simple reply command
+        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
+            player.sendMessage("You said: [accent] " + args[0]);
+        });
+
+        //register a whisper command which can be used to send other players messages
+        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
+            //find player by name
+            Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
+
+            //give error message with scarlet-colored text if player isn't found
+            if(other == null){
+                player.sendMessage("[scarlet]No player by that name found!");
+                return;
+            }
+
+            //send the other player a message, using [lightgray] for gray text color and [] to reset color
+            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+        });
+    }
+}
