397a4c3f99d3cb3f4ec594b474ee8e6671f839e7
alex.mindustry <alex.mindustry@gmail.com>
2020-12-23 14:11:09 +0800

    its not yet working :(

--- src/alexhub/AlexHubPlugin.java
+++ src/alexhub/AlexHubPlugin.java
@@ -1,6 +1,10 @@
 package alexhub;
 
+import arc.Events;
 import arc.util.CommandHandler;
+import mindustry.Vars;
+import mindustry.game.EventType;
+import mindustry.gen.Call;
 import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 
@@ -9,6 +13,10 @@ public class AlexHubPlugin extends Plugin{
     //called when game initializes
     @Override
     public void init(){
+        Events.on(EventType.ServerLoadEvent.class, event -> {
+            Vars.netServer.admins.addActionFilter(playerAction -> false);
+            Call.label(ConfigTranslate.get("server1.title"), 1100f, 25f, 25f);
+        });
     }
 
     //register commands that player can invoke in-game
@@ -19,6 +27,7 @@ public class AlexHubPlugin extends Plugin{
         handler.<Player>register("reply25", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
             player.sendMessage("You said: [accent] " + args[0]);
         });
+
 
     }
 }
